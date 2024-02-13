06c6d4158b9e63ed4967a81384398be307ee8202
jakegodsall <jake.edward.godsall@gmail.com>
2024-02-13 10:20:03 +0000

    added tablet layout to Hero component

--- src/app/(home)/components/Hero/Hero.jsx
+++ src/app/(home)/components/Hero/Hero.jsx
@@ -4,16 +4,18 @@ import heroBackground from "@bg/bg-home-hero.svg";
 
 export default function Hero() {
   return (
-    <section className="bg-home-hero relative h-[40rem] text-center">
-      <h1 className="mb-[1.8rem] px-[2.4rem] text-[4rem] leading-[4rem]">
-        Find the
-        <br /> best <span className="text-primary-coral">talent</span>
-      </h1>
-      <p className="mb-[9.7rem] px-[2.4rem] text-[1.5rem] leading-[2.8rem]">
-        Finding the right people and building high performing teams can be hard.
-        Most companies aren't tapping into the abundance of global talent. We're
-        about to change that.
-      </p>
+    <section className="bg-home-hero relative flex flex-col items-center text-center">
+      <div className="mx-[2.4rem] mb-[20rem] max-w-[45.7rem] sm:mb-[25.6rem]">
+        <h1 className="mb-[1.8rem] text-[4rem] leading-[4rem] sm:mb-[2.4rem] sm:text-[6.4rem] sm:leading-[5.6rem]">
+          Find the
+          <br /> best <span className="text-primary-coral">talent</span>
+        </h1>
+        <p className="text-[1.5rem] leading-[2.8rem] opacity-80">
+          Finding the right people and building high performing teams can be
+          hard. Most companies aren't tapping into the abundance of global
+          talent. We're about to change that.
+        </p>
+      </div>
       <div className="absolute bottom-0">
         <Image src={heroBackground} alt="hero design" />
       </div>
