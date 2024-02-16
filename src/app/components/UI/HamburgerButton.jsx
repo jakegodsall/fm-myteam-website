654dbf3f4257814dc3a4e120d619b4ce1063a3c5
jakegodsall <jake.edward.godsall@gmail.com>
2024-02-16 06:51:15 +0000

    added animation for hamburger button

--- src/app/components/UI/HamburgerButton.jsx
+++ src/app/components/UI/HamburgerButton.jsx
@@ -1,7 +1,29 @@
-export default function HamburgerButton() {
+import clsx from "clsx";
+
+export default function HamburgerButton({ mobileMenuOpen }) {
   return (
-    <div className="relative">
-      <div className="h-[0.3rem] w-[2.3rem] bg-primary-white before:absolute before:top-[-0.8rem] before:h-[0.3rem] before:w-[2.3rem] before:bg-primary-white after:absolute after:top-[0.8rem] after:h-[0.3rem] after:w-[2.3rem] after:bg-primary-white"></div>
+    <div className="relative flex flex-col gap-[0.6rem]">
+      <div
+        className={clsx(
+          mobileMenuOpen
+            ? "h-[0.3rem] w-[2.3rem] translate-y-[0.8rem] rotate-45 bg-primary-white transition-all duration-300"
+            : "h-[0.3rem] w-[2.3rem] bg-primary-white transition-all duration-300",
+        )}
+      ></div>
+      <div
+        className={clsx(
+          mobileMenuOpen
+            ? "h-[0.3rem] w-[2.3rem] bg-primary-white opacity-0 transition-all duration-300"
+            : "h-[0.3rem] w-[2.3rem] bg-primary-white transition-all duration-300",
+        )}
+      ></div>
+      <div
+        className={clsx(
+          mobileMenuOpen
+            ? "h-[0.3rem] w-[2.3rem] translate-y-[-1rem] rotate-[135deg] bg-primary-white transition-all duration-700"
+            : "h-[0.3rem] w-[2.3rem] bg-primary-white transition-all duration-700",
+        )}
+      ></div>
     </div>
   );
 }
